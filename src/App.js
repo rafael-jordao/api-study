c186c173b66c66859d731d4cea531eeb68dbf195
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-04-08 11:00:57 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+//cadastrar produtos com nome preço e cores
+// renderizar produtos acima de 1500 reais
+// utilizar style backgroundColor nas divs
+
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766']
+  },
+
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  }
+]
+
+
+
+const App = () => {
+
+  const dados = produtos.filter(
+    ({ preco }) => Number(preco.replace('R$', '')) > 1500
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {dados.map(({ id, nome, preco, cores }) => <div key={id}>
+        <h1>{nome}</h1>
+        <p>Preço: {preco}</p>
+        <ul>
+          
+          {cores.map((cor) => (
+            <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
+              {cor}
+            </li>
+          ))}
+       
+        </ul>
+      </div>)}
+   
     </div>
   );
-}
+};
 
 export default App;
